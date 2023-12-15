069614af44d3a4fa60d10dc21114a1a6507d2f14
Timothy Burnsed <143354694+tburnsed@users.noreply.github.com>
2023-12-15 13:45:25 -0500

    Create Owner avatar PS-WindowsApp-Bloat-Remover.ps1

---
+++ Owner avatar PS-WindowsApp-Bloat-Remover.ps1
@@ -0,0 +1,14 @@
+
+$location = "C:\Program Files\WindowsApps"
+
+#Take Ownsership of Files in WindowsApps
+takeown -r -f $location
+
+#Give NTFS security permissons to Local Admin group
+$acl = Get-Acl $location
+$Ar = New-Object System.Security.AccessControl.FileSystemAccessRule("BUILTIN\Administrators", "FullControl", "ContainerInherit,ObjectInherit", "None", "Allow")
+$Acl.SetAccessRule($Ar)
+Set-Acl $location $Acl
+
+#Remove All Files
+Get-ChildItem -Path $location -Include *.* -File -Recurse | foreach { $_.
